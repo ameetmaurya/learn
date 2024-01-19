a991ea82d9c55ceaf25ecf57ade1e370ce1c54b4
ameetmaurya <ameetmaurya19@gmail.com>
2024-01-19 20:56:40 +0530

    reverseString

---
+++ java_question_practice/reverseString.java
@@ -0,0 +1,31 @@
+package java_question_practice;
+
+
+public class reverseString {
+    public static void main(String[] args) {
+        
+     String name = "Ameet";
+     String  rstr=  "";
+
+
+     System.out.println(name.charAt(0));
+     for(int i = 0;i<name.length();i++)
+     {
+        
+        System.out.println(name.charAt(i));
+
+        rstr =  name.charAt(i)+rstr;
+       
+        
+     }
+
+     System.out.println("your name reverseString is:");
+
+  
+     System.out.println(rstr);
+
+
+
+    }
+    
+}
