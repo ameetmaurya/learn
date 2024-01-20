4a94a66e7645aef518d0c09bd5b4c17113574beb
ameetmaurya <ameetmaurya19@gmail.com>
2024-01-20 21:30:18 +0530

    CheckVowel

---
+++ java_question_practice/CheckVowel/vowel.java
@@ -0,0 +1,26 @@
+package java_question_practice.CheckVowel;
+import java.util.*;
+public class vowel {
+
+    public static void main(String[] args) {
+       // char[] vowel = {'a','e','i','o','u'};
+        
+      System.out.println("Enter any word to check wheather Vowel is present or not");
+    Scanner sc = new Scanner(System.in);
+    String name = sc.next();
+
+ for(int i = 0;i<name.length();i++)
+ {
+    if(name.charAt(i)=='a'||name.charAt(i)== 'e'|| name.charAt(i)== 'i'|| name.charAt(i)== 'o' ||name.charAt(i)== 'u')
+    {
+        // System.out.println("Given String contain Vowel"+ name.charAt(i)+"at index"+i);
+        System.out.println(name.charAt(i)+"  vowel is present at index  "+ i);
+           
+    
+ } 
+  
+
+    
+}
+}
+}
