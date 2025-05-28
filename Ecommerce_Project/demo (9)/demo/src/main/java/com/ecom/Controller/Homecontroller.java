13eb4372d2b4ae1f52c6eb5954ee92c7b84b53ad
ameetmaurya <ameetmaurya19@gmail.com>
2025-05-28 16:35:44 +0530

    project updates

--- Ecommerce_Project/demo (9)/demo/src/main/java/com/ecom/Controller/Homecontroller.java
+++ Ecommerce_Project/demo (9)/demo/src/main/java/com/ecom/Controller/Homecontroller.java
@@ -24,5 +24,10 @@ public class Homecontroller {
     public  String products(){
         return "product";
     }
+    @GetMapping("/product")
+    public  String product(){
+        return "view_product";
+    }
+
 
 }
